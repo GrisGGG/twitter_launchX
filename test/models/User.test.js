6ed198ef8019a880a89fca2a365c88288f305265
GrisGGG <astropoetscapicua@gmail.com>
2022-04-28 14:53:44 -0500

    3. Modificación de la clase usuario, agregando valores por defecto con new Date()

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,16 +3,15 @@ const User = require('./../../app/models/User')
 describe("Unit Test for User class", () =>{
     test('Create an User object', () =>{
         //Aquí se invoca el código que usara la app
-        const user = new User(1, "GrisGGG", "Gris", "Bio", "dateCreated", "lastUpdated");
+        const user = new User(1, "GrisGGG", "Gris", "Bio");
 
         //Aquí se validadn los resultados de ese código
         expect(user.id).toBe(1)
         expect(user.username).toBe("GrisGGG")
         expect(user.name).toBe("Gris")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
-
+        expect(user.dateCreated).not.toBeUndefined() //not.toBeUndefined() verifica que el valor no sea undefine o vacio
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
 
