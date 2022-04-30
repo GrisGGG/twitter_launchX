55324e90dba8c561532e008771e60b2766ff0681
GrisGGG <astropoetscapicua@gmail.com>
2022-04-30 10:29:00 -0500

    6. Creación del método getInfo

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,8 +1,13 @@
 const User = require('./../models/User')
-
+ //Requeriment 1
 class UserService{
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+    //Requeriment 3
+    static getInfo(user){
+        return [user.id, user.username, user.name, user.bio, user.dateCreated, user.lastUpdate]
+    }
+    //Requeriment 3
 }
 module.exports = UserService
